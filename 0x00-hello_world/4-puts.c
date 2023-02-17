d63f55c750ec44cb01b98da5216de2714aa2af72
EmmaKenny1 <kemshut@gmail.com>
2023-02-16 20:07:19 -0800

    Size is not grandeur

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio>
 /**
- * main - Program printing a line with a puts function
+ * main - A Program that print a line with puts function
  * Return:0 (Succes)
  */
 int main(void)
-(
+{
  puts("\"Programming is like building a multilingual puzzle");
  return (0);
-)
+ }
