2f4f1ff709031933cb7ad575a23d67517039d3ac
EmmaKenny1 <kemshut@gmail.com>
2023-02-16 18:09:48 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+ * main - Program printing a line with a puts function
+ * Return:0 (Succes)
+ */
+int main(void)
+(
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0);
+)
