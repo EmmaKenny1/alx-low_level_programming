64357f76ce547eeef30242ee1a6de191f053364d
EmmaKenny1 <kemshut@gmail.com>
2023-02-22 04:23:44 -0800

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-#include <unixstd.h>
+#include <unistd.h>
 /**
- * main - A C program that print a line to standar error
- * Return 1 (Success)
- */
+* main - main function in the task
+*
+*
+*Return: return 1
+*/
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
